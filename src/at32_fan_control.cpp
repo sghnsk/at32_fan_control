d2d777a99392dbea4d0a47afbb86c7c1c7aa412d
sgh <sgh.nsk@gmail.com>
2019-07-18 01:31:23 +0700

    добавил пробное чтение с 2-х кналов с датчиками ds18b20

--- src/at32_fan_control.cpp
+++ src/at32_fan_control.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 // Include the library
 #include <FanController.h>
+#include <OneWire.h>
 
 // Sensor wire is plugged into port 2 on the Arduino.
 // For a list of available pins on your board,
@@ -17,27 +18,70 @@
 #define PWM_PIN_1 5
 #define PWM_PIN_2 6
 
+#define DS_1 7
+#define DS_2 8
+
 // Initialize library
 FanController fan1 = FanController(SENSOR_PIN_1, SENSOR_THRESHOLD, PWM_PIN_1);
 FanController fan2 = FanController(SENSOR_PIN_2, SENSOR_THRESHOLD, PWM_PIN_2);
 
-/*
-   The setup function. We only start the library here
-*/
+OneWire ds1(DS_1);  // Dallas one wire data buss pin, a 4.7K resistor pullup is needed
+OneWire ds2(DS_2);  // Dallas one wire data buss pin, a 4.7K resistor pullup is needed
+
 void setup(void) {
     // start serial port
     Serial.begin(9600);
-    Serial.println("Fan Controller Library Demo");
 
     // Start up the library
     fan1.begin();
     fan2.begin();
 }
 
-/*10
-   Main function, get and show the temperature
-*/
+int16_t GetMaxTemperature(OneWire *Sensor) {
+	byte data[12];
+	int16_t raw = 0;
+	int16_t celsius = 0;
+	int16_t maxValue = 0;
+	
+	byte addr[8];
+	
+	Sensor->reset_search();
+	
+	while(Sensor->search(addr)) {
+		Sensor->reset();       // reset one wire buss
+		Sensor->select(addr);
+		Sensor->write(0x44);   // start conversion
+		// delay(1000);                     // wait for the conversion 
+		Sensor->reset();
+		// Sensor->skip();
+		Sensor->select(addr);
+		Sensor->write(0xBE);   // Read Scratchpad
+		for (byte i = 0; i < 9; i++) {       // 9 bytes
+			data[i] = Sensor->read();
+		} 
+		// Convert the data to actual temperature
+		raw = (data[1] << 8) | data[0];
+		celsius = raw / 16;
+				
+		// обновить максимальное значение
+		if (celsius > maxValue) {
+			maxValue = celsius;
+		}
+	}
+	Sensor->reset_search();	
+	return maxValue;
+}
+
 void loop(void) {
+    Serial.print("ds1: ");
+    int temp = GetMaxTemperature(&ds1);
+    Serial.print(temp);
+    Serial.println(" C");
+	Serial.print("ds2: ");
+	temp = GetMaxTemperature(&ds2);
+    Serial.print(temp);
+    Serial.println(" C");
+    
     // Call fan.getSpeed() to get fan RPM.
     Serial.print("Current speed - 1: ");
     unsigned int rpms = fan1.getSpeed(); // Send the command to get RPM
